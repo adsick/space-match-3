eb80a0f3986751daea21adc7a9e8442dcfeb824b
adsick <adsick@protonmail.com>
2025-06-03 15:44:12 +0300

    ignore old demo files

--- src/demo/mod.rs
+++ src/demo/mod.rs
@@ -1,3 +1,5 @@
+#![allow(unused)]
+
 //! Demo gameplay. All of these modules are only intended for demonstration
 //! purposes and should be replaced with your own game logic.
 //! Feel free to change the logic found here if you feel like tinkering around
