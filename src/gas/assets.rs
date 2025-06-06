71e4460b96857eb11ee809f8820129e15053c547
adsick <adsick@protonmail.com>
2025-06-06 21:38:55 +0300

    add red

--- src/gas/assets.rs
+++ src/gas/assets.rs
@@ -42,9 +42,9 @@ impl FromWorld for OrbAssets {
         }));
 
         orb_materials.push(assets.add(StandardMaterial {
-            base_color: RED.with_alpha(0.5).into(),
+            base_color: RED.with_alpha(0.7).into(),
             alpha_mode: AlphaMode::Blend,
-            emissive: (RED * 0.3).mix(&GOLD, 0.3).into(),
+            emissive: (RED * 0.5).mix(&GOLD, 0.3).into(),
             ..Default::default()
         }));
 
